99e78983d83b5c2f82f296bcecbbe6329996f412
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-20 16:04:23 -0600

    working on getting cards built

---
+++ Puppy-Bowl/src/components/PuppyDetails.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const PuppyDetails=(singlePuppyDetails, setShowDetails)=>{
+const [singlePuppyDetails, setSinglePuppyDetails]=useState({});
+
+useEffect(()=>{
+  const getSinglePuppy= async()=>{
+    const response = await fetch(singlePuppyDetails);
+    const singlePuppy = await response.json();
+    setSinglePuppyDetails(singlePuppy);
+  }
+  getSinglePuppy();
+}, [])
+
+return(
+  <>
+  <h1>Puppy Details</h1>
+  {
+    <p>{singlePuppyDetails.name}</p>
+  }
+  </>
+)
+}
--- Puppy-Bowl/src/components/PuppyList.jsx
+++ Puppy-Bowl/src/components/PuppyList.jsx
@@ -1,26 +1,40 @@
 import { useEffect, useState } from "react";
 
 const PuppyList = () => {
-
-const [allPuppyList , setAllPuppyList] = useState([])
+  const [allPuppyList, setAllPuppyList] = useState([]);
 
   useEffect(() => {
-    
-      const getPuppies = async() => {
+    const getPuppies = async () => {
       const response = await fetch(
         `https://fsa-puppy-bowl.herokuapp.com/api/2310-fsa-et-web-ft-sf/players`
       );
       const results = await response.json();
-      console.log(results);
-      const allPuppies = results.results;
+      // console.log("results", results);
+      const allPuppies = results.data.players;
+      console.log("allPuppies", allPuppies);
       setAllPuppyList(allPuppies);
-      console.log(allPuppyList);
+      // console.log(allPuppyList);
     };
     getPuppies();
-  },[]);
+  }, []);
 
   return (
-  <h1>Puppy List</h1>);
+    <>
+      <h1>Puppy List</h1>
+      {console.log(allPuppyList)}
+      {
+        allPuppyList.map((singlePuppy)=>{
+          return(
+            <div onClick={()=>{
+              setShowDetails(true);
+              setSinglePuppyImgUrl(allPuppies.data.players.imageURL);
+            }} key={singlePuppy.name}>{singlePuppy.name}
+            </div>
+          )
+        })
+      }
+
+    </>
+  );
 };
 export default PuppyList;
-
